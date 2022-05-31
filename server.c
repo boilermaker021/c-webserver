696caf802c29fefde14579c631123c9ffe062252
boilermaker021 <nathan.leakeas@gmail.com>
2022-05-31 04:44:54 -0500

    rudimentary communication with browser

--- server.c
+++ server.c
@@ -11,12 +11,19 @@
 
 void *handle_request(void *ptr) {
   int client_fd = *((int *)ptr);
-  char buf[100] = {0};
-  sprintf(buf, "Your file descriptor is %d", client_fd);
-  send(client_fd, buf, sizeof(buf), 0);
-  while (1) {
-    int i = 1;
-  }
+  char buf[2048] = {0};
+  recv(client_fd, buf, sizeof(buf), 0);
+  printf("%s\n", buf);
+  
+  char header[1024] = {0};
+  sscanf(buf, "%63[^\n]", header);
+  char url[512] = {0};
+  sscanf(header, "GET %511[^ ] HTTP", url);
+
+  char response[2048] = {0};
+  sprintf(response, "HTTP/1.1 200 OK\nContent-Length:1024\r\n\r\n <h1>You are visiting %s</h1>", url);
+  send(client_fd, response, sizeof(response), 0);
+  
 }
 
 
@@ -24,15 +31,25 @@ int main() {
   int status;
   struct sockaddr_in address;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(5000);
+  address.sin_port = htons(5902);
   address.sin_family = AF_INET;
 
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    printf("Socket creation error");
+  }
 
-  bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+  status = bind(sockfd, (struct sockaddr *)&address, sizeof(address));
 
-  listen(sockfd, 4);
+  if (status < 0) {
+    printf("bind error");
+  }
+
+  status = listen(sockfd, 4);
+  if (status < 0) {
+    printf("listen error");
+  }
   pthread_t *thread_array = malloc(sizeof(pthread_t) * 4);
   unsigned int thread_counter = 0;
   unsigned int thread_array_size = 4;
